1982085ad16f53f566b4cbc16f1dd62522048557
= <=>
2017-03-24 10:45:53 -0400

    test case

---
+++ test/testparsers.py
@@ -0,0 +1,24 @@
+'''
+Created on March 7, 2017
+Copyright (c) 2017
+Harvard FAS Research Computing
+All rights reserved.
+
+@author: Aaron Kitzmiller
+'''
+
+import unittest
+
+class Test(unittest.TestCase):
+    '''
+    Test the findPalindromes method
+    '''
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testBlastParser(self):
+        pass
+~
